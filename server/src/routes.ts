2ee97500d597122f9e2520536354ee8749a30583
VInicyus <vinicyus346@gmail.com>
2020-07-15 03:06:01 -0300

    Back Pronto

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,18 @@
-import express from 'express';
+// "npm run dev"
+
+
+import express, { request, response } from 'express';
+import PointsController from './controllers/points_controller';
+import ItemsController from './controllers/items_controller';
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Hello World'});
-});
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create); 
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
